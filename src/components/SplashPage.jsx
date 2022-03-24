22494ed6eb4ed39fb2a1717b29afbad295f758c8
Aiden Blinn <apblinn@gmail.com>
2022-03-24 17:06:29 -0500

    port entry form and electron handling

--- src/components/SplashPage.jsx
+++ src/components/SplashPage.jsx
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import { ListItemIcon, ListItemText, MenuItem } from '@mui/material';
+import { Button, ListItemText, MenuItem, TextField } from '@mui/material';
+import { ipcRenderer } from 'electron';
 
 class SplashPage extends Component {
   constructor() {
     super();
   }
 
+  handlePortEntry(event) {
+    event.preventDefault();
+    const port = document.getElementById('port').value;
+    document.getElementById('port').value = '';
+    ipcRenderer.send('port:add', port);
+  }
+
   render() {
     return (
-      <>
+      <center>
         <MenuItem
           onClick={this.props.toggleSidebar}
           component={Link}
@@ -17,7 +25,13 @@ class SplashPage extends Component {
         >
           <ListItemText primary="Go to home" />
         </MenuItem>
-      </>
+        <form onSubmit={this.handlePortEntry}>
+          <TextField id="port" label="Enter port" variant="outlined" />
+          <Button type="submit" variant="contained" color="primary">
+            Submit
+          </Button>
+        </form>
+      </center>
     );
   }
 }
