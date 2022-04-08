04d2cf43f4c002aacd75656d51b249b6f5ded6bb
Aiden Blinn <apblinn@gmail.com>
2022-04-07 21:44:02 -0500

    finish config generator

--- configs/jmx_exporter/metric_list.js
+++ configs/jmx_exporter/metric_list.js
@@ -1,3 +1,5 @@
+// list of metrics user can choose from with their corresponding
+// jmx-exporter metrics to be exposed to prometheus
 module.exports = {
   broker_hard_disk_usage: {
     global_topics_size: ['kafka.log:*', 'kafka.cluster:*'],
@@ -18,9 +20,40 @@ module.exports = {
     ],
     request_rate: ['kafka.network:type=RequestMetrics,*'],
     queue_size: ['kafka.network:type=RequestChannel,*'],
+    queue_time: ['kafka.network:type=RequestMetrics,*'],
+    time_placeholder: ['kafka.network:type=RequestMetrics,*'],
   },
-  broker_zookeeper: {},
-  cluster_healthcheck: {},
-  cluster_replication: {},
-  topics_logs: {},
+  broker_zookeeper: {
+    zookeeper_metrics: [
+      'kafka.server:type=SessionExpireListener,*',
+      'kafka.server:type=KafkaServer,name=BrokerState',
+    ],
+  },
+  cluster_healthcheck: {
+    core_healthcheck: [
+      'kafka.controller:*',
+      'kafka.server:type=ReplicaManager,*',
+      'kafka.cluster:*',
+      'kafka.server:type=KafkaServer,name=BrokerState',
+      'kafka.network:type=RequestMetrics,*',
+    ],
+    throughput_io: ['kafka.server:type=BrokerTopicMetrics,*'],
+    isr_count_change: ['kafka.server:type=ReplicaManager,*'],
+    leaders_partitions: [
+      'kafka.controller:*',
+      'kafka.server:type=ReplicaManager,*',
+    ],
+  },
+  cluster_replication: {
+    replication_io: ['kafka.server:type=BrokerTopicMetrics,*'],
+    replication_lag: ['kafka.server:type=ReplicaFetcherManager,*'],
+    replica_fetcher: ['kafka.server:type=FetcherStats,*'],
+  },
+  topics_logs: {
+    log_info: [
+      'kafka.log:*',
+      'kafka.cluster:*',
+      'kafka.server:type=BrokerTopicMetrics,*',
+    ],
+  },
 };
