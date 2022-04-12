e287d7473e0e0735ed100b9eb93371524f1bfbff
Ritchie Cervantes <ritchie.cervantes@gmail.com>
2022-04-12 16:54:43 -0500

    Render hard disk usage and modularize out implementation

---
+++ src/components/BrokerHardDiskUsage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import metricURLs from '../models/metricURLs';
+import renderMetricPanels from './_utils/renderMetricPanels';
+
+function BrokerHardDiskUsage(props) {
+    const {metrics} = props;
+    return (
+        <>
+            <h1>Broker Hard Disk Usage</h1>
+            {renderMetricPanels(metrics, metricURLs.broker_hard_disk_usage)}
+        </>
+    );
+}
+
+export default BrokerHardDiskUsage;
