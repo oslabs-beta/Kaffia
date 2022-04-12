9c1ef8ad8034619306fbca725d672ced53f7388d
Ritchie Cervantes <ritchie.cervantes@gmail.com>
2022-04-12 17:07:13 -0500

    Update routes and route attributes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,18 @@
 import React, { Component } from 'react';
 import { HashRouter, Route, Routes, useLocation } from 'react-router-dom';
+import {ipcRenderer} from 'electron';
 
-const Brokers = React.lazy(() => import('./Brokers'));
-const Consumers = React.lazy(() => import('./Consumers'));
-const Producers = React.lazy(() => import('./Producers'));
-const Topics = React.lazy(() => import('./Topics'));
-const HelpTab = React.lazy(() => import('./HelpTab'));
-
+import BrokerHardDiskUsage from './BrokerHardDiskUsage';
+import BrokerJVMAndOS from './BrokerJVMOS';
+import Consumers from './Consumers';
+import Producers from './Producers';
+import Topics from './Topics';
+import HelpTab from './HelpTab';
 import Cluster from './Cluster';
 import Overview from './Overview';
 import Performance from './Performance';
 import Settings from './Settings';
 import Sidebar from './Sidebar';
-import { ipcRenderer } from 'electron';
-
 class App extends Component {
   constructor() {
     super();
@@ -27,65 +26,70 @@ class App extends Component {
   }
 
   componentDidUpdate() {
-    console.log(this.state);
+    console.log("this.state.metrics: ", this.state.metrics);
+    // console.log("metricProps: ", metricProps);
   }
+  
 
   render() {
+    let broker_hard_disk_usage, broker_jvm_os, broker_performance, broker_zookeeper, cluster_healthcheck, cluster_replication, topics_logs;
+    if(this.state.metrics) {
+      ({broker_hard_disk_usage, broker_jvm_os, broker_performance, broker_zookeeper, cluster_healthcheck, cluster_replication, topics_logs} = this.state.metrics);
+    }
+
     return (
       <HashRouter>
         <Sidebar />
         <main style={{ marginLeft: '250px' }}>
+          {this.state.metrics &&
           <Routes>
             <Route exact path="/" element={<Overview />} />
             <Route exact path="/cluster" element={<Cluster />} />
             <Route
               exact
-              path="/brokers"
-              element={
-                <React.Suspense fallback={<>...</>}>
-                  <Brokers />
-                </React.Suspense>
-              }
+              path="/brokerHardDiskUsage"
+              // element={<BrokerHardDiskUsage metrics={broker_hard_disk_usage} />}
             />
             <Route
               exact
-              path="/consumers"
-              element={
-                <React.Suspense fallback={<>...</>}>
-                  <Consumers />
-                </React.Suspense>
-              }
+              path="/brokerJVMOS"
+              element={<BrokerJVMAndOS metrics={broker_jvm_os} />}
             />
             <Route
               exact
-              path="/producers"
-              element={
-                <React.Suspense fallback={<>...</>}>
-                  <Producers />
-                </React.Suspense>
-              }
+              path="/brokerPerformance"
+              element={<Producers />}
             />
             <Route
               exact
-              path="/topics"
-              element={
-                <React.Suspense fallback={<>...</>}>
-                  <Topics />
-                </React.Suspense>
-              }
+              path="/brokerZookeeper"
+              element={<Topics />}
+            />
+            <Route
+              exact
+              path="/clusterHealthcheck"
+              element={<Topics />}
+            />
+            <Route
+              exact
+              path="/clusterReplication"
+              element={<Topics />}
+            />
+            <Route
+              exact
+              path="/topicsLogs"
+              element={<Topics />}
             />
             <Route
               exact
               path="/HelpTab"
-              element={
-                <React.Suspense fallback={<>...</>}>
-                  <HelpTab />
-                </React.Suspense>
-              }
+              element={<HelpTab />}
             />
             <Route exact path="/performance" element={<Performance />} />
             <Route exact path="/settings" element={<Settings />} />
           </Routes>
+          }
+          
         </main>
       </HashRouter>
     );
