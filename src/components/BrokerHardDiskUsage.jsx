735773cf45c3b668cd2b47e8772ebc375005b65d
Aiden Blinn <apblinn@gmail.com>
2022-04-13 15:41:23 -0500

    change content components to display correct grafana links

--- src/components/BrokerHardDiskUsage.jsx
+++ src/components/BrokerHardDiskUsage.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
-import metricURLs from '../models/metricURLs';
 import renderMetricPanels from './_utils/renderMetricPanels';
 
 function BrokerHardDiskUsage(props) {
-    const {metrics} = props;
-    return (
-        <>
-            <h1>Broker Hard Disk Usage</h1>
-            {metrics && renderMetricPanels(metrics, metricURLs.broker_hard_disk_usage)}
-        </>
-    );
+  const { metrics, metricURLs } = props;
+  console.log(props);
+  console.log('hello');
+  return (
+    <>
+      <h1>Broker Hard Disk Usage</h1>
+      {metrics && renderMetricPanels(metrics, metricURLs)}
+    </>
+  );
 }
 
 export default BrokerHardDiskUsage;
---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Dashboard extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Welcome to Kaffia!</h1>
+        
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
