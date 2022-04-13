a85978c7d77a5e8202fb0c084d2ac914011d8753
Ritchie Cervantes <ritchie.cervantes@gmail.com>
2022-04-13 11:29:15 -0500

    Update props

--- src/components/_utils/renderMetricPanels.js
+++ src/components/_utils/renderMetricPanels.js
@@ -4,9 +4,9 @@ import displayMetrics from './displayMetrics';
 
 function renderMetricPanels(metrics, metricPanel) {
     return (
-        <Grid container spacing={10} rowSpacing={9} columnSpacing={3}>
+        <Grid container spacing={10} rowSpacing={5} columnSpacing={3} >
             {metrics.map((metric) => (displayMetrics(metricPanel[metric])))}
-        </Grid>
+        </Grid >
     );
 }
 
