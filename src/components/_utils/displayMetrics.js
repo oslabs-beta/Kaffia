ce9464eeee56674cbccd1905342da6dccc2d5f29
Ritchie Cervantes <ritchie.cervantes@gmail.com>
2022-04-13 11:29:45 -0500

    Refactor and reformat

--- src/components/_utils/displayMetrics.js
+++ src/components/_utils/displayMetrics.js
@@ -5,13 +5,11 @@ import MetricCard from '../MetricCard';
 function displayMetrics(metricCategory) {
     const metricURLArray = Object.values(metricCategory);
     
-    return metricURLArray.map((metricURL, index) => {
-        return (
-            <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
-                <MetricCard metricURL={metricURL} key={`${metricCategory}${metricURL}`} />
-            </Grid>
-        );
-    });
+    return metricURLArray.map((metricURL, index) => (
+        <Grid item xs={12} sm={6} md={4} lg={3} key={index}>
+            <MetricCard metricURL={metricURL} key={`${metricCategory}${metricURL}`} />
+        </Grid >
+    ));
 }
 
 
