e32328a49e13baa7d28dc74dec0b6df874dc3a67
Aiden Blinn <apblinn@gmail.com>
2022-04-13 21:57:08 -0500

    edit metric card size and display

--- src/components/MetricCard.jsx
+++ src/components/MetricCard.jsx
@@ -2,28 +2,33 @@ import React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardActionArea from '@mui/material/CardActionArea';
-import CardMedia from '@mui/material/CardMedia'
+import CardMedia from '@mui/material/CardMedia';
 import Grid from '@mui/material/Grid';
 
 function MetricCard(props) {
-    const {metricURL} = props;
-    return (
-        <>
-            <Card key={`SRC${metricURL}`} elevation={20}  sx={{p:0}}>
-                <CardActionArea>
-                    <CardContent>
-                        {/* <Grid container spacing={6} rowSpacing={0.1} >
-                            <Grid item xs={12} sm={12} md={12} lg={12} justifyContent="space-between"> */}
-                                {/* <iframe src={metricURL} width="100%" height="100%" frameBorder="0" /> */}
-                                <CardMedia component="iframe" src={metricURL} width="100%" height="100%" frameBorder="0" sx={{p:0}}/>
-                            {/* </Grid>
-                        </Grid> */}
-                    </CardContent>
-                </CardActionArea>
-            </Card>
-        </>
-    );
+  const { metricURL } = props;
+  return (
+    <>
+      <Card
+        key={`SRC${metricURL}`}
+        elevation={20}
+        sx={{ p: 0, height: '100%' }}
+      >
+        <CardActionArea sx={{ height: '100%' }}>
+          <CardContent>
+            <CardMedia
+              component="iframe"
+              src={metricURL}
+              width="100%"
+              height="100%"
+              frameBorder="0"
+              sx={{ p: 0, height: '85%' }}
+            />
+          </CardContent>
+        </CardActionArea>
+      </Card>
+    </>
+  );
 }
-
 
 export default MetricCard;
--- src/components/_utils/renderMetricPanels.js
+++ src/components/_utils/renderMetricPanels.js
@@ -3,12 +3,11 @@ import Grid from '@mui/material/Grid';
 import displayMetrics from './displayMetrics';
 
 function renderMetricPanels(metrics, metricPanel) {
-    return (
-        <Grid container spacing={10} rowSpacing={5} columnSpacing={3} >
-            {metrics.map((metric) => (displayMetrics(metricPanel[metric])))}
-        </Grid >
-    );
+  return (
+    <Grid container columnSpacing={4}>
+      {metrics.map((metric) => displayMetrics(metricPanel[metric]))}
+    </Grid>
+  );
 }
-
 
 export default renderMetricPanels;
